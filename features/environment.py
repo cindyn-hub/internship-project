6c3691e3b22b624dea5d4a3c9ddc8f14c46d91b0
cindyn-hub <cindyndebumadu@outlook.com>
2025-07-21 20:32:42 -0400

    Internship Task 3: Jira Ticket# QAAR-1015

--- features/environment.py
+++ features/environment.py
@@ -2,25 +2,39 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
+# from selenium.webdriver.chrome.options import Options
 
 from app.application import Application
 
+
 def browser_init(context):
+
     """
     :param context: Behave context
     """
-    driver_path = ChromeDriverManager().install()
-    service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
+    # driver_path = ChromeDriverManager().install()
+    # service = Service(driver_path)
+    # context.driver = webdriver.Chrome(service=service)
+    context.driver = webdriver.Firefox()
+
+    ### HEADLESS MODE ####
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')  # Enable headless mode
+    # options.add_argument('window-size=1920,1080')  # Set window size to Full HD
+    #
+    # context.driver = webdriver.Chrome(service=service, options=options)
+
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
+    context.driver.wait = WebDriverWait(context.driver, 10)
     context.app = Application(context.driver)
 
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
     browser_init(context)
+    # browser_init(context, scenario.name)
 
 
 def before_step(context, step):
@@ -33,4 +47,5 @@ def after_step(context, step):
 
 
 def after_scenario(context, feature):
+    # context.driver.delete_all_cookies()
     context.driver.quit()
