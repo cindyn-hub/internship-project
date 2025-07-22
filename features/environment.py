a9ce78db17ca736ee4f01301e8b38aae3334ce4f
cindyn-hub <cindyndebumadu@outlook.com>
2025-07-21 21:08:10 -0400

    Internship Task 4: Jira Ticket# QAAR-1037

--- features/environment.py
+++ features/environment.py
@@ -2,12 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options
 
 from app.application import Application
 
 
-def browser_init(context):
+# def browser_init(context):
+def browser_init(context, scenario_name):
 
     """
     :param context: Behave context
@@ -15,13 +16,28 @@ def browser_init(context):
     # driver_path = ChromeDriverManager().install()
     # service = Service(driver_path)
     # context.driver = webdriver.Chrome(service=service)
-    context.driver = webdriver.Firefox()
+    # context.driver = webdriver.Firefox()
 
     ### HEADLESS MODE ####
     # options = webdriver.ChromeOptions()
     # options.add_argument('headless')
     # options.add_argument('window-size=1920,1080')
     # context.driver = webdriver.Chrome(service=service, options=options)
+
+    # BROWSERSTACK ###
+    bs_user ='***'
+    bs_key = '***'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        "os" : "Windows",
+        "osVersion" : "11",
+        'browserName': 'Chrome',
+        'sessionName': scenario_name,
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
 
     context.driver.maximize_window()
@@ -32,7 +48,7 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario.name)
     # browser_init(context, scenario.name)
 
 
@@ -45,6 +61,6 @@ def after_step(context, step):
         print('\nStep failed: ', step)
 
 
-def after_scenario(context, feature):
+def after_scenario(context, scenario):
     # context.driver.delete_all_cookies()
     context.driver.quit()
--- pages/left_side_menu.py
+++ pages/left_side_menu.py
@@ -10,6 +10,6 @@ class LeftSideMenu(Page):
     # OFF_PLAN_BTN = (By.CSS_SELECTOR, 'a[class="menu-button-block w-inline-block"]')
 
     def click_off_plan_button(self):
-        # self.wait_for_element_click(*self.OFF_PLAN_BTN)
-        sleep(3)
-        self.click(*self.OFF_PLAN_BTN)
+        self.wait_for_element_click(*self.OFF_PLAN_BTN)
+        # sleep(3)
+        # self.click(*self.OFF_PLAN_BTN)
